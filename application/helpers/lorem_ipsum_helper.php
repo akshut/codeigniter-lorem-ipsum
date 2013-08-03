79e1ce8f378668fa88715dfd969a14931cfb5b62
rkchaux@gmail.com <rkchaux@gmail.com>
2013-08-03 09:58:05 +0530

    added default values

--- application/helpers/lorem_ipsum_helper.php
+++ application/helpers/lorem_ipsum_helper.php
@@ -13,12 +13,12 @@
  * Lorem Ipsum Generator
  *
  * @param   integer Number of paragraphs to generate
- * @param   size of text in paras - small, medium, large
+ * @param   size of text in paras - short, medium, large
  * @param   options array - see readme
  * @return  html with Lorem Ipsum text
  */
 if (!function_exists('lipsum')) {
-    function lipsum($no_paras, $size, $options= array())
+    function lipsum($no_paras=1, $size='short', $options= array())
     {
         $contents = file_get_contents('http://loripsum.net/api/' . $no_paras . '/' . $size . join('/', $options));
         return $contents;
